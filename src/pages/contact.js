0bc2cfec4ca40f786c8d316210c04ff9017c69a4
Clint Losee <clint.losee@gmail.com>
2018-10-11 09:31:38 -0600

    Fixed missing import

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { Contact } from '../components/Contact';
-import { Contact2 } from '../components/Contact2';
 
 const contact = () => {
   return (
