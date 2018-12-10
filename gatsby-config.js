d85d2c40efd273a2cb4613a2c8c0619374950424
Clint Losee <clint.losee@gmail.com>
2018-12-10 13:52:05 -0700

    Changed site icon for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'clint-losee-digital-portfolio',
-        short_name: 'starter',
+        short_name: 'CL-Portfolio',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#ff6600',
+        theme_color: '#ff6600',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/clintlosee-icon.png' // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-offline',
