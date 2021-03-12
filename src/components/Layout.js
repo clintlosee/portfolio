927cae44e1f59f8e3d094c997f9b54777742d37e
Clint Losee <clint.losee@gmail.com>
2021-03-12 14:18:41 -0700

    Change layout name

---
+++ src/components/Layout.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import { StaticQuery, graphql } from 'gatsby';
+
+import Header from './header';
+// import { Welcome } from './Welcome';
+// import BG from '../../public/static/assets/images/bg-pattern.svg';
+// import 'semantic-ui-css/semantic.min.css';
+import './layout.css';
+
+const theme = {
+  red: '#FF0000',
+  orange: '#FF6600',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1200px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'Roboto';
+    src: url('/public/static/Roboto-Regular.ttf') format('ttf');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px !important;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem !important;
+    line-height: 2;
+    font-family: 'Roboto', sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <ThemeProvider theme={theme}>
+        <>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' },
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <StyledPage>
+            <Inner>{children}</Inner>
+          </StyledPage>
+        </>
+      </ThemeProvider>
+    )}
+  />
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-import Layout from '../components/layout';
-import { Welcome } from '../components/Welcome';
+import Layout from '../components/Layout'
+import { Welcome } from '../components/Welcome'
 
 const IndexPage = () => (
   <Layout>
     <Welcome />
   </Layout>
-);
+)
 
-export default IndexPage;
+export default IndexPage
